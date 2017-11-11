e879f14f263e56816fea8486676add8c15aa3ecf
Evgeniy Polyakov <zbr@ioremap.net>
2017-11-11 19:14:31 +0300

    Added main/follower networks

--- network.py
+++ network.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
 import numpy as np
 
+def get_param_name(s):
+    return s.split('/', 1)[1].replace('/', 'X').split(':')[0]
+def get_scope_name(s):
+    return s.split('/')[0].split(':')[0]
+def get_transform_placeholder_name(s):
+    return get_param_name(s) + '_ext'
+
 class network(object):
-    def __init__(self, config):
+    def __init__(self, scope, config):
+        self.scope = scope
         self.summary_writer = config.get('summary_writer')
         self.summary_update_steps = config.get('summary_update_steps')
         self.train_steps = 0
@@ -31,15 +39,43 @@ class network(object):
         self.dense = tf.layers.dense(inputs=flat, units=config.get('dense_layer_units'),
                 activation=tf.contrib.keras.layers.PReLU(alpha_initializer=tf.constant_initializer(prelu_alpha)),
                 use_bias=True, name='dense_layer',
+                kernel_regularizer=tf.nn.l2_loss,
                 kernel_initializer=kinit,
                 bias_initializer=tf.random_normal_initializer(0, 0.1))
 
         self.output = tf.layers.dense(inputs=self.dense, units=actions, use_bias=True, name='output_layer',
                 kernel_initializer=kinit,
+                kernel_regularizer=tf.nn.l2_loss,
                 bias_initializer=tf.random_normal_initializer(0, 0.1))
 
-        self.loss = tf.reduce_mean(tf.square(self.output - qvals))
+        reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)) * 0
+        mse = tf.reduce_mean(tf.square(tf.stop_gradient(self.output) - qvals))
+        self.loss = tf.add(mse, reg_loss)
         self.summary_all.append(tf.summary.scalar('loss', self.loss))
+
+        self.transform_variables = []
+        self.assign_ops = []
+        for v in tf.trainable_variables():
+            if self.scope != get_scope_name(v.name):
+                continue
+
+            ev = tf.placeholder(tf.float32, None, name=get_transform_placeholder_name(v.name))
+            self.assign_ops.append(tf.assign(v, ev, validate_shape=False))
+
+            self.transform_variables.append(v)
+
+        for i in range(actions):
+            x = tf.one_hot(i, actions)
+            out = self.output * x
+            sum = tf.reduce_sum(out, axis=-1)
+            self.summary_all.append(tf.summary.scalar("qvals_pred_{0}".format(i), tf.reduce_mean(sum)))
+
+            out_in = qvals * x
+            sum_in = tf.reduce_sum(out_in, axis=-1)
+            self.summary_all.append(tf.summary.scalar("qvals_{0}".format(i), tf.reduce_mean(sum_in)))
+
+        self.summary_all.append(tf.summary.histogram("qvals", qvals))
+        self.summary_all.append(tf.summary.histogram("qvals_pred", self.output))
 
         self.global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
         self.learning_rate = 0.00001 + tf.train.exponential_decay(config.get('learning_rate_start'), self.global_step,
@@ -67,8 +103,8 @@ class network(object):
 
     def train(self, states, qvals):
         ret = self.session.run([self.summary_merged, self.optimizer_step, self.global_step], feed_dict={
-                'states:0': states,
-                'qvals:0': qvals,
+                self.scope + '/states:0': states,
+                self.scope + '/qvals:0': qvals,
             })
 
         self.train_steps += 1
@@ -79,7 +115,40 @@ class network(object):
 
     def predict(self, states):
         ret = self.session.run([self.output], feed_dict={
-                'states:0': states,
+                self.scope + '/states:0': states,
             })
 
         return ret[0]
+
+    def export_params(self):
+        res = self.session.run(self.transform_variables)
+        d = {}
+        for k, v in zip(self.transform_variables, res):
+            d[k.name] = v
+        return d
+
+    def import_params(self, d, self_rate):
+        def name(name):
+            return self.scope + '/' + get_transform_placeholder_name(name) + ':0'
+
+        ext_d = {}
+        for k, ext_v in d.iteritems():
+            ext_d[name(k)] = ext_v
+
+            #if 'policy_layer/kernel' in k:
+            #    print "exported {0}: name: {1}, value: {2}".format(k, name(k), ext_v)
+
+        import_d = {}
+        for k, self_v in self.export_params().iteritems():
+            tn = name(k)
+
+            ext_var = ext_d.get(tn, self_v)
+
+            import_d[tn] = self_v * self_rate + ext_var * (1. - self_rate)
+
+            #if 'policy_layer/kernel' in k:
+            #    print "import: scope: {0}, name: {1}, self_rate: {2}, self_v: {3}, ext_var: {4}, saving: {5}".format(
+            #            self.scope, tn, self_rate, self_v, ext_var, import_d[tn])
+
+        #print("{0}: imported params: {1}, total params: {2}".format(self.scope, len(d), len(d1)))
+        self.session.run(self.assign_ops, feed_dict=import_d)
