791ebf7fc05294b66299952998d52747d972bd01
Evgeniy Polyakov <zbr@ioremap.net>
2017-11-14 13:10:12 +0300

    qlearn: added wrappers from openai baseline

--- qlearn.py
+++ qlearn.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from copy import deepcopy
 
+import collections
 import cv2
 import gym
 import random
@@ -12,13 +13,57 @@ import history
 import network
 import state
 
-import config as _config
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env=None):
+        """For environments where the user need to press FIRE for the game to start."""
+        super(FireResetEnv, self).__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def _reset(self):
+        self.env.reset()
+        obs, _, done, _ = self.env.step(1)
+        if done:
+            self.env.reset()
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset()
+        return obs
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only every `skip`-th frame"""
+        super(MaxAndSkipEnv, self).__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = collections.deque(maxlen=2)
+        self._skip = skip
+
+    def _step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            self._obs_buffer.append(obs)
+            total_reward += reward
+            if done:
+                break
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+        return max_frame, total_reward, done, info
+
+    def _reset(self):
+        """Clear past frame buffer and init. to first obs. from inner env."""
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+        return obs
 
 class qlearn(object):
     def __init__(self, config):
         self.total_steps = 0
 
         self.env = gym.make(config.get('game'))
+        self.env = MaxAndSkipEnv(self.env)
+        self.env = FireResetEnv(self.env)
 
         self.input_shape = config.get('input_shape')
         self.state_steps = config.get('state_steps')
@@ -109,7 +154,7 @@ class qlearn(object):
             states[idx] = s.read()
             next_states[idx] = sn.read()
 
-        qvals = self.follower.predict(states)
+        qvals = self.main.predict(states)
         next_qvals = self.follower.predict(next_states)
 
         #print("q1: {}".format(qvals))
